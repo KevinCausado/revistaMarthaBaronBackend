2f0ca5e35caa62b03a427198d34121c3156cc6d2
KingGladiatore <developerkevin32412@gmail.com>
2024-10-29 12:13:09 -0500

    HW-73, Modulo de Seguridad: Implmentacion guardado registro en mayusculas(categoria), faltan los demas y cuando actuaiiza

--- db/models/entities/Categoria.js
+++ db/models/entities/Categoria.js
@@ -20,7 +20,18 @@ class Categoria extends Model {
       modelName: 'Categoria',
       tableName: 'categoria',
       schema: process.env.DB_SCHEMA,
-      paranoid: true
+      paranoid: true,
+      hooks: {
+        beforeSave: (instance) => {
+          Object.keys(instance.rawAttributes).forEach((field) => {
+            // console.log(`Campo: ${field} , Tipo de dato: ${instance.rawAttributes[field].type}`)
+            const fieldType = instance.rawAttributes[field].type;
+            if (fieldType == 'VARCHAR(255)' || fieldType == 'TEXT') {
+              instance[field] = instance[field].toUpperCase();
+            }
+          });
+        }
+      }
     }
   }
 }
@@ -41,7 +52,7 @@ const CategoriaSchema = {
         if (this.isnewRecord || value !== undefined) {
           const fieldName = Object.keys(this.rawAttributes).find(key => this.getDataValue(key) === value);
           if (value === '') {
-            throw new AppError(`The field '${fieldName}' cannot be empty`,400);
+            throw new AppError(`The field '${fieldName}' cannot be empty`, 400);
           }
         }
       }
@@ -56,7 +67,7 @@ const CategoriaSchema = {
         if (this.isnewRecord || value !== undefined) {
           const fieldName = Object.keys(this.rawAttributes).find(key => this.getDataValue(key) === value);
           if (value === '') {
-            throw new AppError(`The field '${fieldName}' cannot be empty`,400);
+            throw new AppError(`The field '${fieldName}' cannot be empty`, 400);
           }
         }
       }
